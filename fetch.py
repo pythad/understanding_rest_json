1dbc3b6f2b8bae31c10f763d8d14a8cf5f67a4d9
Vladyslav Ovchynnykov <ovd@ovdlaptop.inc>
2015-12-20 16:46:11 +0200

    str exception added; argparse -> input;  data dict -> OrderedDict

--- fetch.py
+++ fetch.py
@@ -1,34 +1,35 @@
 #!/usr/bin/env python3
 """
-Usage:
-
-python3 fetch.py "<user_id>"(0 < user_id <= 100)
-
-Example:
-
-python3 fetch.py 21
-
 Requirements:
 
 requests (installation: pip install requests)
 """
-
-import argparse
+from collections import OrderedDict
 import requests
 import json
 
-parser = argparse.ArgumentParser(description='Process the query input')
-parser.add_argument('user_id', type=int, help='user_id to get data for')
-args = parser.parse_args()
-user_id = args.user_id
+prompt = """
+The script fetches data from http://jsonplaceholder.typicode.com/posts/<user_id>
+input user_id, it has to be an integer (0 < user_id <= 100): 
+"""
 
-while not 0 < user_id <= 100:
-    user_id = int(input("Please, provide correct data (0 < user_id <= 100): "))
+print(prompt)
+
+while True:
+    try:
+        user_id = int(input())
+    except ValueError:
+        print('user_id has to be a number')
+    else:
+        if 0 < user_id <= 100:
+            break
+        else:
+            print('Please, provide correct data (0 < user_id <= 100)')
 
 FETCH_URL = 'http://jsonplaceholder.typicode.com/posts/' + str(user_id)
 
 resp = requests.get(url=FETCH_URL)
-data = json.loads(resp.text)
-print('\n')
+data = json.loads(resp.text, object_pairs_hook=OrderedDict)
+print()
 for key, value in data.items():
     print("{}: {}".format(key, value))
